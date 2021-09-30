7a9297368820e3232542554666bb1f1eb8f1408c
Michael Chacaton <personal@chaksoft.fr>
2021-09-30 21:52:49 +0200

    add gun handler

--- peer.js
+++ peer.js
@@ -1,5 +1,5 @@
 const http = require('http')
 const Gun = require('gun')
 
-const server = http.createServer().listen(parseInt(process.env.PORT || '8080', 10))
+const server = http.createServer(Gun.serve(__dirname)).listen(parseInt(process.env.PORT || '8080', 10))
 Gun({ web : server })
